183ebec78eaf5c511ac75a8fdbaeafbcbd88d465
teojincheng <jinchengproject@gmail.com>
2019-04-04 22:20:03 +0800

    Add my own modifications

--- app/src/androidTest/java/com/simplemobiletools/calculator/ApplicationTest.kt
+++ app/src/androidTest/java/com/simplemobiletools/calculator/ApplicationTest.kt
@@ -1,6 +1,6 @@
 package com.simplemobiletools.calculator
 
 import android.app.Application
-import android.test.ApplicationTestCase
+//import android.test.ApplicationTestCase
 
-class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
+//class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
---
+++ app/src/androidTest/java/com/simplemobiletools/calculator/MTest.kt
@@ -0,0 +1,40 @@
+package com.simplemobiletools.calculator
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import com.simplemobiletools.calculator.activities.MainActivity
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class MTest {
+    @Test
+    fun appLaunchesSuccessfully() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+
+    @Test
+    fun someFun(){
+        ActivityScenario.launch(MainActivity::class.java);
+
+        /*
+        onView(withId(R.id.settings))
+                .perform(click())
+*/
+
+        //onView(withContentDescription(R.string.settings)).perform(click());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Settings")).perform(click());
+
+        onView(withId(R.id.settings_holder)).check(matches(isCompletelyDisplayed()));
+
+
+    }
+}
