01ea250f7b1598839c210209b0248ecd5483e267
teojincheng <jinchengproject@gmail.com>
2019-04-06 20:17:43 +0800

    change unit test such that it should pass

--- app/src/androidTest/java/com/simplemobiletools/calculator/MTest.kt
+++ app/src/androidTest/java/com/simplemobiletools/calculator/MTest.kt
@@ -33,8 +33,8 @@ public class MTest {
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         onView(withText("Settings")).perform(click());
 
-        //onView(withId(R.id.settings_holder)).check(matches(isCompletelyDisplayed()));
-        onView(withId(R.id.config_bg_color)).check(matches(isCompletelyDisplayed()));
+        onView(withId(R.id.settings_holder)).check(matches(isCompletelyDisplayed()));
+
 
 
 
